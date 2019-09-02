fcf6c9b096421fb1b82f354fdf6b0b76de9dfbb6
Jenn Eng <jenneng6@gmail.com>
2019-09-02 14:38:40 -0500

    starting point

---
+++ app/models/course.rb
@@ -0,0 +1,9 @@
+class Course < ApplicationRecord
+  belongs_to :professor
+  has_many :reviews, dependent: :destroy
+
+  validates :professor, presence: true
+  validates :name, presence: true
+  validates :catalog_number, presence: true
+  validates :description, presence: true
+end
